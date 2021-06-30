7fb847abbf4d707376f9766aaf513a6ae1378b66
Valens Nsengiman <nsengvalens4@gmai.com>
2021-06-30 09:43:35 +0200

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./index.css";
 import App from "./App";
 import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
